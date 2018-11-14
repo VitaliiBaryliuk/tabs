c574e036821e408e27b7001c2e02494dc79ad696
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-11-14 20:21:20 +0200

    Uploaded files

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+(function () {
+  const links = document.querySelectorAll('.navigation__link'); 
+
+  for(let i = 0; i < links.length; i++){
+    links[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            //console.log(links[i].dataset.link);
+            for(let j = 0; j < links.length; j++){
+              if(i == j){
+                document.querySelector('.'+links[j].dataset.link).classList.remove('invisible');
+            }
+            else{
+              document.querySelector('.'+links[j].dataset.link).classList.add('invisible');
+            }
+          }
+      });
+  }
+
+}());
