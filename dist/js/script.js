297c8cd5316deec9afd1f8cd719a5e06ef2eab9f
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-11-15 23:43:01 +0200

    All code changes directly by review propositions

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+(function () {
+  const tabLink = document.querySelector('.tabs__navigation');
+  const tabWraps = document.querySelectorAll('div[data-tabcontent]');
+
+  tabLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    const dataLink = event.target.dataset.link;
+    for (let i = 0; i < tabWraps.length; i++) {
+      tabWraps[i].dataset.tabcontent === dataLink ? tabWraps[i].classList.remove('hide') : tabWraps[i].classList.add('hide');
+    }
+  });
+}());
