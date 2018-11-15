4805da61d8f0c8ef0654cdb00f7a38cd05ed8265
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-11-15 17:16:17 +0200

    Update index.js

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,7 +4,6 @@
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', (event) => {
       event.preventDefault();
-      // console.log(links[i].dataset.link);
       for (let j = 0; j < links.length; j++) {
         if (i == j) {
           document.querySelector(`.${links[j].dataset.link}`).classList.remove('invisible');
