fea9d244c0230c8a3748fd1d6bfeeef1955093f2
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-11-16 10:50:25 +0200

    Fixed menu bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 (function () {
   const tabLink = document.querySelector('.tabs__navigation');
+  const tabLinks = document.querySelectorAll('.tabs__link');
   const tabWraps = document.querySelectorAll('div[data-tabcontent]');
   document.querySelector('a[data-link="home"]').focus();
 
   tabLink.addEventListener('click', (event) => {
     event.preventDefault();
     const dataLink = event.target.dataset.link;
-    for (let i = 0; i < tabWraps.length; i++) {
+    for (let i = 0; i < tabWraps.length; i += 1) {
+      console.log(tabLinks[i]);
+      tabLinks[i].dataset.link === dataLink ? event.target.classList.add('show') : tabLinks[i].classList.remove('show'); 
       tabWraps[i].dataset.tabcontent === dataLink ? tabWraps[i].classList.remove('hide') : tabWraps[i].classList.add('hide');
     }
   });
